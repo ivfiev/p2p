3d33a0f2b5544fa3e9cd0861ccf11849440f19c5
ivfiev <filjko@ya.ru>
2024-09-10 21:56:11 +0100

    strsplit util

--- src/utils.c
+++ src/utils.c
@@ -2,13 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 void trim_end(char *str) {
-  while (*str) {
-    if (*str == '\n') {
-      *str = 0;
-    }
-    str++;
+  while (isspace(*str)) {
+    *str-- = '\0';
   }
 }
 
@@ -28,8 +27,19 @@ size_t hash_str(void *ptr, size_t N) {
   char c;
   while ((c = *str++)) {
     i += i * 89 + (int)c;
+    i %= N;
   }
-  return i % N;
+  return i;
+}
+
+size_t hash_strs(void **ptr, size_t count, size_t N) {
+  size_t hash = 0;
+  for (int i = 0; i < count; i++) {
+    size_t h = hash_str(ptr[i], N);
+    hash = h + hash + h * hash;
+    hash %= N;
+  }
+  return hash;
 }
 
 double randf() {
@@ -56,4 +66,17 @@ void timestamp(char *buf) {
   tm_info = localtime(&tv.tv_sec);
   size_t c = strftime(buf, 12, "%H:%M:%S", tm_info);
   snprintf(buf + c, 19, ".%03ld", tv.tv_usec / 1000);
+}
+
+size_t strsplit(char *str, const char *sep, char **buf) {
+  size_t i = 0;
+  char *save_ptr;
+  char *tmp = strtok_r(str, sep, &save_ptr);
+  for (buf[i++] = tmp; tmp != NULL;) {
+    tmp = strtok_r(NULL, sep, &save_ptr);
+    if (tmp != NULL) {
+      buf[i++] = tmp;
+    }
+  }
+  return i;
 }
