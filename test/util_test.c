d2b7729817550a967ea74aef69e38a977cd7b206
ivfiev <filjko@ya.ru>
2024-09-10 21:56:25 +0100

    util tests

---
+++ test/util_test.c
@@ -0,0 +1,36 @@
+#include "test.h"
+#include "fakes.h"
+#include "hash.h"
+#include "util.h"
+
+TEST(str_hash_test) {
+  size_t h1 = hash_str("Test1", 117);
+  size_t h2 = hash_str("Test2", 117);
+  ASSERT(h1 > 0 && h2 > 0 && h1 != h2);
+}
+
+TEST(str_split_test_commas) {
+  char str[128];
+  char *buf[16];
+  strcpy(str, "hello,world");
+  size_t i = strsplit(str, ",", buf);
+  ASSERT_EQ(2, (int)i);
+  ASSERT_STR_EQ("hello", buf[0]);
+  ASSERT_STR_EQ("world", buf[1]);
+}
+
+TEST(str_split_test_newlines) {
+  char str[128];
+  char *buf[16];
+  strcpy(str, "hello\nworld\n\n");
+  size_t i = strsplit(str, "\n", buf);
+  ASSERT_EQ(2, (int)i);
+  ASSERT_STR_EQ("hello", buf[0]);
+  ASSERT_STR_EQ("world", buf[1]);
+}
+
+void util_test_run(void) {
+  RUN_TEST(str_hash_test);
+  RUN_TEST(str_split_test_commas);
+  RUN_TEST(str_split_test_newlines);
+}
